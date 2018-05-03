2f9c1127709204c7cac53f8c1c9e182847e0bd12
Tusalika <yangrunze777@gmail.com>
2018-05-03 20:04:05 +0800

    helf done success

--- app/src/main/java/com/dict/android/MainActivity.java
+++ app/src/main/java/com/dict/android/MainActivity.java
@@ -9,22 +9,203 @@ import com.dict.android.db.WordsSQLiteOpenHelper;
 
 import org.litepal.LitePal;
 
-public class MainActivity extends AppCompatActivity {
+//public class MainActivity extends AppCompatActivity {
+//
+//    private WordsSQLiteOpenHelper dbHelper;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        dbHelper=new WordsSQLiteOpenHelper(this,"Dictionary.db",null,1);
+//        Button createDatabase=(Button) findViewById(R.id.create_database);
+//        createDatabase.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                dbHelper.getWritableDatabase();
+////                LitePal.getDatabase();
+//            }
+//        });
+//    }
+//}
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.SearchView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-    private WordsSQLiteOpenHelper dbHelper;
+import com.dict.android.model.Words;
+import com.dict.android.R;
+import com.dict.android.util.HttpCallBackListener;
+import com.dict.android.util.HttpUtil;
+import com.dict.android.util.ParseXML;
+import com.dict.android.util.WordsAction;
+import com.dict.android.util.WordsHandler;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+
+/**
+ * Created by YRZ on 2018/5/3.
+ * 查词界面
+ */
+public class MainActivity extends Activity {
+    private SearchView searchView;
+    private TextView searchWords_key, searchWords_psE, searchWords_psA, searchWords_posAcceptation, searchWords_sent;
+    private ImageButton searchWords_voiceE, searchWords_voiceA;
+    private LinearLayout searchWords_posA_layout,searchWords_posE_layout, searchWords_linerLayout, searchWords_fatherLayout;
+    private WordsAction wordsAction;
+
+    private Words words = new Words();
+    /**
+     * 网络查词完成后回调handleMessage方法
+     */
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case 111:
+                    //判断网络查找不到该词的情况
+                    if (words.getSent().length() > 0) {
+                        upDateView();
+                    } else {
+                        searchWords_linerLayout.setVisibility(View.GONE);
+                        Toast.makeText(MainActivity.this, "抱歉！找不到该词！", Toast.LENGTH_SHORT).show();
+                    }
+                    Log.d("测试", "tv保存2");
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        dbHelper=new WordsSQLiteOpenHelper(this,"Dictionary.db",null,1);
-        Button createDatabase=(Button) findViewById(R.id.create_database);
-        createDatabase.setOnClickListener(new View.OnClickListener() {
+        wordsAction = WordsAction.getInstance(this);
+        //初始化控件
+        searchWords_linerLayout = (LinearLayout) findViewById(R.id.searchWords_linerLayout);
+        searchWords_posA_layout = (LinearLayout) findViewById(R.id.searchWords_posA_layout);
+        searchWords_posE_layout = (LinearLayout) findViewById(R.id.searchWords_posE_layout);
+        searchWords_fatherLayout = (LinearLayout) findViewById(R.id.searchWords_fatherLayout);
+        searchWords_fatherLayout.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                dbHelper.getWritableDatabase();
-//                LitePal.getDatabase();
+            public void onClick(View v) {
+                //点击输入框外实现软键盘隐藏
+                InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                inputMethodManager.hideSoftInputFromWindow(v.getWindowToken(), 0);
             }
         });
+        searchWords_key = (TextView) findViewById(R.id.searchWords_key);
+        searchWords_psE = (TextView) findViewById(R.id.searchWords_psE);
+        searchWords_psA = (TextView) findViewById(R.id.searchWords_psA);
+        searchWords_posAcceptation = (TextView) findViewById(R.id.searchWords_posAcceptation);
+        searchWords_sent = (TextView) findViewById(R.id.searchWords_sent);
+        searchWords_voiceE = (ImageButton) findViewById(R.id.searchWords_voiceE);
+        searchWords_voiceE.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                wordsAction.playMP3(words.gettext(), "E", MainActivity.this);
+            }
+        });
+        searchWords_voiceA = (ImageButton) findViewById(R.id.searchWords_voiceA);
+        searchWords_voiceA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                wordsAction.playMP3(words.gettext(), "A", MainActivity.this);
+            }
+        });
+        searchView = (SearchView) findViewById(R.id.searchWords_searchView);
+        searchView.setSubmitButtonEnabled(true);//设置显示搜索按钮
+        searchView.setIconifiedByDefault(false);//设置不自动缩小为图标
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                loadWords(query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+
+    }
+
+    /**
+     * 读取words的方法，优先从数据中搜索，没有在通过网络搜索
+     */
+    public void loadWords(String key) {
+        words = wordsAction.getWordsFromSQlite(key);
+        if ("" == words.gettext()) {
+            String address = wordsAction.getAddressForWords(key);
+            HttpUtil.sentHttpRequest(address, new HttpCallBackListener() {
+                @Override
+                public void onFinish(InputStream inputStream) {
+                    WordsHandler wordsHandler = new WordsHandler();
+                    ParseXML.parse(wordsHandler, inputStream);
+                    words = wordsHandler.getWords();
+                    wordsAction.saveWords(words);
+                    wordsAction.saveWordsMP3(words);
+                    handler.sendEmptyMessage(111);
+                }
+
+                @Override
+                public void onError() {
+
+                }
+            });
+        } else {
+            upDateView();
+        }
+    }
+
+    /**
+     * 更新UI显示
+     */
+    public void upDateView() {
+        if (words.getIsChinese()) {
+            searchWords_posAcceptation.setText(words.getFy());
+            searchWords_posA_layout.setVisibility(View.GONE);
+            searchWords_posE_layout.setVisibility(View.GONE);
+        } else {
+            searchWords_posAcceptation.setText(words.getPosAcceptation());
+            if(words.getPsE()!="") {
+                searchWords_psE.setText(String.format(getResources().getString(R.string.psE), words.getPsE()));
+                searchWords_posE_layout.setVisibility(View.VISIBLE);
+            }else {
+                searchWords_posE_layout.setVisibility(View.GONE);
+            }
+            if(words.getPsA()!="") {
+                searchWords_psA.setText(String.format(getResources().getString(R.string.psA), words.getPsA()));
+                searchWords_posA_layout.setVisibility(View.VISIBLE);
+            }else {
+                searchWords_posA_layout.setVisibility(View.GONE);
+            }
+        }
+        searchWords_key.setText(words.gettext());
+        searchWords_sent.setText(words.getSent());
+        searchWords_linerLayout.setVisibility(View.VISIBLE);
+    }
+
+    //加载actionbar的菜单
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.actionbar_layout_menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
