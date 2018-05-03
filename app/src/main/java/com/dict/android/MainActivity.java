bd2a6a92e4c6da969fe5782ad369109e9faa41eb
Tusalika <yangrunze777@gmail.com>
2018-05-03 17:36:13 +0800

    helf done

--- app/src/main/java/com/dict/android/MainActivity.java
+++ app/src/main/java/com/dict/android/MainActivity.java
@@ -2,12 +2,29 @@ package com.dict.android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.dict.android.db.WordsSQLiteOpenHelper;
+
+import org.litepal.LitePal;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WordsSQLiteOpenHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dbHelper=new WordsSQLiteOpenHelper(this,"Dictionary.db",null,1);
+        Button createDatabase=(Button) findViewById(R.id.create_database);
+        createDatabase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dbHelper.getWritableDatabase();
+//                LitePal.getDatabase();
+            }
+        });
     }
 }
